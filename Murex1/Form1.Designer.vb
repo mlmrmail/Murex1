128e8d8215d155b52d890d5a40cb823e31a6c6f0
mlmrmail <mlmrmail@gmail.com>
2017-06-05 10:08:30 +0300

    testing comminting to the branch

--- Murex1/Form1.Designer.vb
+++ Murex1/Form1.Designer.vb
@@ -22,9 +22,32 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgv
+        '
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgv.Location = New System.Drawing.Point(0, 0)
+        Me.dgv.Name = "dgv"
+        Me.dgv.RowTemplate.Height = 24
+        Me.dgv.Size = New System.Drawing.Size(1370, 691)
+        Me.dgv.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1370, 691)
+        Me.Controls.Add(Me.dgv)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents dgv As DataGridView
 End Class
--- Murex1/Form1.vb
+++ Murex1/Form1.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Form1
 
+
+    Private Sub Form1_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+
+        With dgv
+            .Columns.Add("colDate", "DATE")
+            .Columns.Add("colInstrument", "INSTRUMENTS")
+            .Columns.Add("colPortfolio", "PORTFOLIO")
+            .Columns.Add("colFlows", "FLOWS")
+        End With
+
+
+
+
+    End Sub
+
+
+
 End Class
